7faa640c320206ae734c75936edcf8e3153e40e8
shimyubin <tla1161@naver.com>
2026-01-22 18:26:01 +0900

    [Bronze V] Title: X보다 작은 수, Time: 96 ms, Memory: 11548 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.js
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.js
@@ -2,15 +2,9 @@ const fs = require('fs');
 
 const input = fs.readFileSync(0, 'utf8').trim().split('\n').map(line => line.trim().split(/\s+/).map(Number));
 
-const N = input[0][0];
 const X = input[0][1];
+const A = input[1];
 
-let output = "";
+const resultArr = A.filter(num => num < X);
 
-for (let i = 0; i < N; i++) {
-    if (input[1][i] < X) {
-        output += input[1][i] + " ";
-    }
-}
-
-console.log(output);
+console.log(resultArr.join(' '));
